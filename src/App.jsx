3e761746d3e60693943a569a07133c52bc0b098c
mahboob <mahboob6550@gmail.com>
2025-06-02 12:45:49 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 
 
 function App() {
  
   // const apiUrl =`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`; 
   //              this api is not working 
+  const [country,setCountry]= useState("");
   
   const apiUrl = `https://api.frankfurter.app/latest?from=${country}`;
 
